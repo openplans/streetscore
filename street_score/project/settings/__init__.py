2e96c9925aef4f9c9b9b9602e3e580d029c07f5b
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-01-25 10:56:26 -0500

    Change the static and media roots to something dotcloud can handle

--- street_score/project/settings/__init__.py
+++ street_score/project/settings/__init__.py
@@ -96,7 +96,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/dotcloud/data/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -107,7 +107,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/dotcloud/data/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
