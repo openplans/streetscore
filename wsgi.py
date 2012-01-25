b6ec6cb23b24c277edf409d57c18742fe8e8cd5b
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-01-25 10:16:37 -0500

    Add street_score to the python path in the wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'street_score.project.settings'
+
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'street_score'))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
