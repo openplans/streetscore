d4061ae969d918ff28052013d75a6a9d67b75c6e
Aaron Ogle <atogle@gmail.com>
2012-02-08 11:51:09 -0500

    add django compressor

--- street_score/project/settings/__init__.py
+++ street_score/project/settings/__init__.py
@@ -126,6 +126,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'compressor.finders.CompressorFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -183,6 +184,7 @@ INSTALLED_APPS = (
     'backbonejs',
     'debug_toolbar',
     'django_nose',
+    'compressor',
 
     # Project-specific apps
     'project',
