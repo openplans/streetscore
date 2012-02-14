10900f25c508c38a7d6adfab4b87450276b905fc
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-02-14 15:58:42 -0500

    Turn on list_select_related for the Rating model admin manager so that the generation of the list in admin doesn't make separate queries for each rating

--- street_score/project/admin.py
+++ street_score/project/admin.py
@@ -1,6 +1,9 @@
 from django.contrib.gis import admin
 from . import models
 
+class RatingAdmin (admin.ModelAdmin):
+    list_select_related = True
+
 admin.site.register(models.Criterion)
-admin.site.register(models.Rating)
+admin.site.register(models.Rating, RatingAdmin)
 admin.site.register(models.Segment, admin.GeoModelAdmin)
