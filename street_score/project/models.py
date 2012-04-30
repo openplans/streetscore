f2de4b5d93ced8ce31ab205bbd7d7657995263a3
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-04-30 11:04:45 -0400

    Removed Block-based code from models module

--- street_score/project/models.py
+++ street_score/project/models.py
@@ -42,19 +42,9 @@ class Rating (TimeStampedModel):
             0:  'as {0} as',
         })
 
-        return ('Segment #{s1}, block #{b1} is {rating}'
-                ' segment #{s2}, block #{b2}').format(
-                s1=self.segment1.id, b1=self.block1_index,
-                s2=self.segment2.id, b2=self.block2_index,
+        return ('Place #{p1} is {rating} place #{p2}').format(
+                p1=self.place1, p2=self.place2,
                 rating=meaning[self.score].format(self.criterion.prompt))
-
-    @property
-    def block(self):
-        """
-        The block that this rating scores. The block is identified by the
-        segment and block_index for the rating.
-        """
-        return Block(self.segment, self.block_index)
 
     @property
     def question(self):
@@ -76,38 +66,6 @@ class Criterion (models.Model):
         verbose_name_plural = "criteria"
 
 
-class Segment (models.Model):
-    id = models.IntegerField(db_column='osm_id', primary_key=True)
-    way = models.LineStringField(srid=900913, null=True)
-
-    objects = models.GeoManager()
-
-    class Meta:
-        db_table = u'philly_street_osm_line'
-        managed = False
-
-    def __unicode__(self):
-        return 'Segement #{0}'.format(self.id)
-
-    @property
-    def blocks(self):
-        """
-        The blocks that fall along the segment.  If the segment is shorter than
-        the block length, it will contain only one block.
-        """
-        class BlockSet (object):
-            def __init__(self, segment):
-                self.segment = segment
-
-            def __len__(self):
-                return int(math.ceil(self.segment.way.length / Block.MAX_LENGTH))
-
-            def __getitem__(self, n):
-                return Block(self.segment, n)
-
-        return BlockSet(self)
-
-
 class Place (models.Model):
     lat = models.FloatField()
     lon = models.FloatField()
@@ -116,43 +74,14 @@ class Place (models.Model):
         return '({0}, {1})'.format(self.lat, self.lon)
 
 
-class Block (object):
-    MAX_LENGTH = 200.0
-
-    def __init__(self, segment, index):
-        self.segment = segment
-        self.index = index
-
-    @property
-    def _start_interpolation(self):
-        return (self.MAX_LENGTH * self.index) / self.segment.way.length
-
-    @property
-    def length(self):
-        return min(self.segment.length - (self.index * self.MAX_LENGTH), self.MAX_LENGTH)
-
-    @property
-    def characteristic_point(self):
-        """
-        The a point that will represent the block (not necessarily uniquely).
-        """
-        point_data = Segment.objects.raw(
-            ('SELECT *, ST_transform(ST_line_interpolate_point(way, %s), 4326) '
-                 'AS q_start_point '
-             'FROM philly_street_osm_line WHERE osm_id=%s'),
-            [self._start_interpolation, self.segment.id])[0].q_start_point
-        point = GEOSGeometry(point_data)
-        return point
-
-
 class SurveySession (object):
     """
 
     """
 
-    def __init__(self, questions=None, blocks=None):
+    def __init__(self, questions=None, places=None):
         self.__questions = questions
-        self.__blocks = blocks
+        self.__places = places
 
     @property
     def questions(self):
@@ -162,23 +91,23 @@ class SurveySession (object):
         return self.__questions or self.init_questions()
 
     @property
-    def blocks(self):
+    def places(self):
         """
         Get the block for this session.
         """
-        return self.__blocks or self.init_blocks()
+        return self.__places or self.init_places()
 
-    def init_blocks(self):
+    def init_places(self):
         """
-        Load two blocks at random.
+        Load two places at random.
 
-        TODO: Order the blocks by those that have the least questions answered
+        TODO: Order the places by those that have the least questions answered
               about them first.
         """
-        segments = Segment.objects.all().order_by('?')[:2]
+        places = Place.objects.all().order_by('?')[:2]
 
-        self.__blocks = [random.choice(segment.blocks) for segment in segments]
-        return self.__blocks
+        self.__places = places
+        return self.__places
 
     def init_questions(self):
         """
@@ -193,14 +122,16 @@ class SurveySession (object):
 
     @classmethod
     def make_surveys(cls, count=1):
-        segments = list(Segment.objects.all().order_by('?')[:(count * 2)])
+        # TODO: Choose the places and questions more smartly.  Use the init_...
+        #       methods defined above (and make them better too).
+        places = list(Place.objects.all().order_by('?')[:(count * 2)])
         questions = list(Criterion.objects.all())
         surveys = []
 
         for i in range(count):
-            block1 = random.choice(segments[2 * i].blocks)
-            block2 = random.choice(segments[2 * i + 1].blocks)
+            place1 = places[2 * i]
+            place2 = places[2 * i + 1]
 
-            surveys.append(cls(blocks=[block1, block2], questions=questions))
+            surveys.append(cls(blocks=[place1, place2], questions=questions))
 
         return surveys
