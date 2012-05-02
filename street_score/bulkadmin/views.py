85f90534785f23ef8d83c3f8a791cc8643a5e0a5
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-05-02 18:30:19 -0400

    Use the admin_urlname function to get the urlspec name for the changelist from the bulk add form

--- street_score/bulkadmin/views.py
+++ street_score/bulkadmin/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.core.urlresolvers import reverse
 from django.contrib.admin.helpers import AdminForm
+from django.contrib.admin.templatetags.admin_urls import admin_urlname
 from django.contrib.admin.util import get_deleted_objects, model_ngettext
 from django.views import generic as views
 from django.utils.encoding import force_unicode
@@ -31,10 +32,15 @@ class BulkUploadFormAdminView (views.FormView):
     def get_context_data(self, *args, **kwargs):
         context = super(BulkUploadFormAdminView, self).get_context_data(*args, **kwargs)
 
+        # We're constructing an AdminForm from the view's form so that we can
+        # use the admin templates that are shipped with Django out of the box.
+        # I used code from django.contrib.admin.options as an example for the
+        # following lines.
         form = context['form']
         fields = form.base_fields.keys()
         fieldsets = [(None, {'fields': fields})]
         bulkadminform = AdminForm(form, fieldsets, {}, model_admin=self)
+
         opts = self.model_admin.model._meta
         context.update({
             'title': u"Add many {0} from a CSV file".format(force_unicode(opts.verbose_name_plural)),
@@ -43,7 +49,8 @@ class BulkUploadFormAdminView (views.FormView):
         return context
 
     def get_success_url(self):
-        return '/admin/project/place/'
+        opts = self.model_admin.model._meta
+        return reverse(admin_urlname(opts, 'changelist'))
 
     def dispatch(self, request, model_admin=None, *args, **kwargs):
         self.model_admin = model_admin
